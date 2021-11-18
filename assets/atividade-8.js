429afb55350c864ad87e4e9f3ac985f43d9e2c5a
João Augusto Carbonera <joaocarbonera06@gmail.com>
2021-11-17 22:32:59 -0300

    Atividade 8-9 feitas, Produto Final

---
+++ assets/atividade-8.js
@@ -0,0 +1,7 @@
+var string1 = prompt('Digite um numero, e se prepare para a rima 😎');
+
+var setesete = parseInt(string1) + parseInt(string1);
+var daleste = parseInt(string1) + parseInt(setesete);
+
+document.getElementById('Texto').innerHTML = ((string1) +' mais '+(string1)+' é '+(setesete));
+document.write('mais '+(string1)+' é = '+ (daleste) +' ruivin, mandando o recado 😎')
---
+++ assets/atividade-9.js
@@ -0,0 +1,5 @@
+document.getElementById('Texto').innerHTML = (0)
+
+function clica(){
+    document.getElementById("Texto").innerHTML = parseInt(document.getElementById("Texto").innerHTML) + 1
+}
